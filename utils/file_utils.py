9a624fee2505d7447fc05a07a71c3369333e7d17
kzzZia <krnanale@my.cspc.edu.ph>
2025-05-20 19:43:55 +0800

    Update file_utils.py
    Allow File Uploads on Render.  Supports file uploads, but files saved to disk are temporary.

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,8 +1,9 @@
 # utils/file_utils.py
 import os
 
-def save_uploaded_file(uploaded_file, folder='uploads'):
+def save_uploaded_file(uploaded_file, folder='/tmp'):
     os.makedirs(folder, exist_ok=True)
     path = os.path.join(folder, uploaded_file.filename)
     uploaded_file.save(path)
     return path
+
